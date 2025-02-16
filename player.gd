a936d9b18e300e1cd9e8e0077bf15a5f7f90a801
namelessblunder <nameless.blunder19@gmail.com>
2025-02-16 13:28:13 +0530

    Changing the ground MESH

--- player.gd
+++ player.gd
@@ -3,6 +3,7 @@ extends CharacterBody3D
 @export var speed = 5.0
 @export var jump_velocity = 4.5
 @export var mouse_sensitivity = 0.1
+@export var sprint_multiplier = 5
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -17,14 +18,20 @@ func _physics_process(delta):
 		velocity.y = jump_velocity
 	
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var is_sprint = Input.is_action_pressed("sprint")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
+	var move_speed = speed
+	
+	if is_sprint:
+		move_speed = speed * sprint_multiplier
+	
 	if direction:
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
+		velocity.x = direction.x * move_speed
+		velocity.z = direction.z * move_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.z = move_toward(velocity.z, 0, speed)
+		velocity.x = move_toward(velocity.x, 0, move_speed)
+		velocity.z = move_toward(velocity.z, 0, move_speed)
 	
 	move_and_slide()
 	
